dde7264e1bb62e2b5bf3fc70b55516666bd61178
Joilsonallves <130906367+Joilsonallves@users.noreply.github.com>
2025-02-20 12:22:51 -0300

    Update

--- js/modulo.js
+++ js/modulo.js
@@ -4,35 +4,35 @@ let senhaCadastrada = "123";
 
 // Ao carregar a página, verifica se há credenciais salvas
 window.onload = () => {
-  if (localStorage.getItem("lembrarDeMim") === "true") {
-    document.getElementById("usuario").value = localStorage.getItem("usuario");
-    document.getElementById("senha").value = localStorage.getItem("senha");
-    document.getElementById("lembrarDeMim").checked = true;
-  }
+    if (localStorage.getItem("lembrarDeMim") === "true") {
+        document.getElementById("usuario").value = localStorage.getItem("usuario");
+        document.getElementById("senha").value = localStorage.getItem("senha");
+        document.getElementById("lembrarDeMim").checked = true;
+    }
 };
 
 function efetuarLogin(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const usuario = document.getElementById("usuario").value;
-  const senha = document.getElementById("senha").value;
-  const lembrarDeMim = document.getElementById("lembrarDeMim").checked;
+    const usuario = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
+    const lembrarDeMim = document.getElementById("lembrarDeMim").checked;
 
-  if (usuario === usuarioCadastrado && senha === senhaCadastrada) {
-    if (lembrarDeMim) {
-      localStorage.setItem("usuario", usuario);
-      localStorage.setItem("senha", senha);
-      localStorage.setItem("lembrarDeMim", true);
+    if (usuario === usuarioCadastrado && senha === senhaCadastrada) {
+        if (lembrarDeMim) {
+            localStorage.setItem("usuario", usuario);
+            localStorage.setItem("senha", senha);
+            localStorage.setItem("lembrarDeMim", true);
+        } else {
+            localStorage.removeItem("usuario");
+            localStorage.removeItem("senha");
+            localStorage.removeItem("lembrarDeMim");
+        }
+        // Redireciona para a página do usuário
+        alert('Login efetuado com sucesso!');
+        location.href = 'paginas/usuario.html';
     } else {
-      localStorage.removeItem("usuario");
-      localStorage.removeItem("senha");
-      localStorage.removeItem("lembrarDeMim");
+        alert("Usuário ou senha incorretos.");
     }
-    // Redireciona para a página do usuário
-    alert('Login efetuado com sucesso!');
-    location.href = 'paginas/usuario.html';
-  } else {
-    alert("Usuário ou senha incorretos.");
-  }
 }
 //FIM DA VALIDAÇÃO DO USUÁRIO
